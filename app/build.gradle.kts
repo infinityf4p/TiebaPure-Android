55a737532fbebb69dea9daa78ee5fac8f9577f1f
CHOS1N11111 <zhuzenan02@gmail.com>
2026-08-28 15:23:48 +0800

    Prepare release 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "dev.infinityf4p.tiebapure"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
